d41de5bcd9ee913ea35c2aab9585b6e97bf98798
Olalakan Otele <lekanbiodun@gmail.com>
2024-07-17 16:35:16 +0100

    updated the responsiveness

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var tablinks = document.getElementsByClassName('tab-links');
 var tabcontents = document.getElementsByClassName('tab-contents');
 
 function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+    for(tablinks of tablinks){
+        tablinks.classList.remove("active-link");
     }
     for(tabcontents of tabcontents){
         tabcontents.classList.remove("active-tab");
     }
-    event.currentTarget.classList.add('active-link');
+    e.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add("active-tab")
 }
  
